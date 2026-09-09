776607df85486e5ae9505e1651dd25087de3b325
Oulhadj Zakaria <69174113+Zakria0@users.noreply.github.com>
2026-09-09 23:08:57 +0100

    Step 21: evaluate_regression

--- model.py
+++ model.py
@@ -222,8 +222,13 @@ def r_squared(y_true, y_pred):
     tot = sum((y_true - np.mean(y_true)) ** 2)
     return 1 - res / tot if tot else np.nan
 
-# Step 21 - evaluate_regression (not yet solved)
-# TODO: implement
+# Step 21 - evaluate_regression
+def evaluate_regression(y_true, y_pred):
+    return {
+        'mae': mean_absolute_error(y_true, y_pred),
+        'rmse': root_mean_squared_error(y_true, y_pred),
+        'r2': r_squared(y_true, y_pred)
+    }
 
 # Step 22 - learning_curve_data (not yet solved)
 # TODO: implement
